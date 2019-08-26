da92da3272185de97600921ecb92c5b00351a1ba
missxa <aliona.kharchenko@gmail.com>
2019-08-26 23:08:17 +0200

    with myobricks spi

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,48 @@
 #include <ESP8266WiFi.h>
 #include <Wire.h>
-#include <ros.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Int16.h>
-#include <std_msgs/Float64.h>
+// #include "ardprintf.h"
+
 #include <Servo.h>
 #include <A1335Utils.h>
+#include <SPI.h>
 
+#include <ros.h>
 #include <sensor_msgs/JointState.h>
 #include <roboy_middleware_msgs/MotorCommand.h>
 #include <roboy_cognition_msgs/RecognizedFaces.h>
+#include <roboy_middleware_msgs/ControlMode.h>
 
 #define HAND_MOTOR_ID 71
 #define WRIST_MOTOR_ID 70
+#define MYOBRICK0_ID 8
+#define MYOBRICK1_ID 9
+
 #define JOINT_NAME "elbow_right"
 #define SERVO_PIN D0
 #define DC_PWM_PIN D3
 #define DC_DIR_PIN D4
-// #define DC_EN_PIN D8
 
-//////////////////////
-// WiFi Definitions //
-//////////////////////
-const char* ssid = "roboyRickshaw";
+#define POSITION 0
+#define VELOCITY 1
+#define DISPLACEMENT 2
+#define DIRECT_PWM 3
+#define maxPWM 500
+
+static const int spiClk = 2000000; // 2 MHz
+
+const int ss_n[2] = {9,10};
+
+float Kp[2] = {0.1,0.1}, Kd[2] = {0.01,0.01}, err[2] = {0,0}, err_prev[2] = {0,0}, myo_setpoint[2] = {0,300}, result[2] = {0,0};
+int control_mode[2] = {0,3};
+
+int32_t position[2] = {0,0};
+int16_t velocity[2] = {0,0}, current[2] = {0,0}, pwmRef[2] = {0,0};
+
+Servo myservo;
+A1335State state;
+int i;
+
+const char* ssid = "roboy";
 const char* password = "wiihackroboy";
 
 IPAddress server(192, 168, 0, 105); // ip of your ROS server
@@ -30,7 +50,6 @@ IPAddress ip_address;
 int status = WL_IDLE_STATUS;
 
 WiFiClient client;
-
 
 class WiFiHardware {
 
@@ -62,24 +81,25 @@ class WiFiHardware {
   }
 };
 
-Servo myservo;
-A1335State state;
-int i;
 
-void chatterCallback(const roboy_middleware_msgs::MotorCommand& msg) {
+void motorCommandCallback(const roboy_middleware_msgs::MotorCommand& msg) {
     for (int i=0; i<(sizeof(msg.motors)/sizeof(*msg.motors)); i++) {
-      if (msg.motors[i] == WRIST_MOTOR_ID) {
-        Serial.print("Got servo setpoint: ");
+      if (msg.motors[i] == MYOBRICK0_ID) {
+        myo_setpoint[0] = msg.set_points[i];
+      }
+      else if (msg.motors[i] == MYOBRICK1_ID) {
+        myo_setpoint[1] = msg.set_points[i];
+      }
+
+      else if (msg.motors[i] == WRIST_MOTOR_ID) {
+        Serial.print("Got servo myo_setpoint: ");
         Serial.println(msg.set_points[i]);
         myservo.write(msg.set_points[i]);
       }
+
       else if (msg.motors[i] == HAND_MOTOR_ID) {
-
-
-        Serial.print("Got DC setpoint: ");
+        Serial.print("Got DC myo_setpoint: ");
         Serial.println(msg.set_points[i]);
-
-
         // digitalWrite(DC_EN_PIN, LOW);
         digitalWrite(DC_PWM_PIN, HIGH);
 
@@ -90,19 +110,54 @@ void chatterCallback(const roboy_middleware_msgs::MotorCommand& msg) {
           digitalWrite(DC_PWM_PIN, LOW);
           digitalWrite(DC_DIR_PIN, LOW);
         }
-        // else if(msg.set_points[i]>)
         else {
             digitalWrite(DC_DIR_PIN, LOW);
         }
 
-        delay(300);
-        // digitalWrite(DC_EN_PIN, HIGH);
+        delay(100);
 
       }
     }
 }
 
-ros::Subscriber<roboy_middleware_msgs::MotorCommand> sub("/roboy/middleware/MotorCommand", &chatterCallback);
+bool controlModeCallback(const roboy_middleware_msgs::ControlMode::Request& req, const roboy_middleware_msgs::ControlMode::Response& res) {
+//     if (req.motor_id == nullptr) {
+//       switch (req.control_mode) {
+//         case POSITION:
+//           for (int i=0; i<2; i++) {
+//             control_mode[i] = POSITION;
+//             myo_setpoint[i] = req.set_point;
+//           }
+//           break;
+//         case DIRECT_PWM:
+//           for (int i=0; i<2; i++) {
+//             control_mode[i] = DIRECT_PWM;
+//             myo_setpoint[i] = req.set_point;
+//           }
+//             break;
+//         default:
+//             Serial.println("Unknown control mode for ESP. Only position or direct_pwm available");
+//             return false;
+//       }
+//     }
+//
+//     else {
+//       for (int i=0; i<(sizeof(req.motor_id)/sizeof(*req.motor_id)); i++) {
+//         if (req.motor_id == MYOBRICK0_ID) {
+//           control_mode[0] = req.control_mode;
+//           myo_setpoint[0] = req.set_point
+//         }
+//         else if (req.motor_id == MYOBRICK1_ID) {
+//             control_mode[1] = req.control_mode;
+//             myo_setpoint[1] = req.set_point
+//           }
+//     }
+//
+// }
+// return true;
+}
+
+ros::Subscriber<roboy_middleware_msgs::MotorCommand> sub("/roboy/middleware/MotorCommand", &motorCommandCallback);
 
 sensor_msgs::JointState rosmsg;
 ros::Publisher elbow_pub("/joint_states", &rosmsg);
@@ -130,11 +185,21 @@ void setup() {
   Serial.begin(115200);
   setupWiFi();
   delay(2000);
+
+  // servo
   myservo.attach(SERVO_PIN);
 
+  // hand DC
   pinMode(DC_PWM_PIN, OUTPUT);
   pinMode(DC_DIR_PIN, OUTPUT);
-  // pinMode(DC_EN_PIN, OUTPUT);
+
+  // myobrick
+  SPI.begin();
+
+  for (int motor=0; motor<2; motor++){
+    pinMode (ss_n[motor], OUTPUT);
+    digitalWrite(ss_n[motor],HIGH);
+  }
 
   int8 length = 1;
 
@@ -161,14 +226,88 @@ void setup() {
   delay(2000);
 }
 
+void myobrickLoop() {
+  for(int motor=0;motor<2;motor++){
+    SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE1));
+    uint16_t data[2];
+    for(int i=0;i<12;i++){
+      digitalWrite(ss_n[motor],LOW);
+      delayMicroseconds(1);
+      if(i==0)
+        SPI.transfer16(0x8000); // header
+      else if(i==1)
+        SPI.transfer16((pwmRef[motor]& 0x7fff));
+      else{
+        switch(i){
+          case 4:
+            data[0] = SPI.transfer16(0);
+            break;
+          case 5:
+            data[1] = SPI.transfer16(0);
+            position[motor] =  ((data[0]>>8)<<24|(data[0]&0xff)<<16|(data[1]>>8)<<8|(data[1]&0xff));
+            break;
+          case 6:
+            velocity[motor] = SPI.transfer16(0);
+            break;
+          case 7:
+            current[motor] = SPI.transfer16(0);
+            break;
+          default:
+            SPI.transfer16(0);
+           break;
+         }
+      }
+      digitalWrite(ss_n[motor],HIGH);
+    }
+    SPI.endTransaction();
+  }
+  // controller
+  for(int motor=0;motor<2;motor++){
+    switch(control_mode[motor]){
+      case POSITION:
+        err[motor] = myo_setpoint[motor]-position[motor];
+        break;
+      case VELOCITY:
+        err[motor] = myo_setpoint[motor]-velocity[motor];
+        break;
+      case DISPLACEMENT: // not implemented
+        err[motor] = 0;
+        break;
+      case DIRECT_PWM:
+        result[motor] = myo_setpoint[motor];
+        break;
+    }
+    if(control_mode[motor]!=DIRECT_PWM){
+      result[motor] = Kp[motor]*err[motor] + Kd[motor]*(err_prev[motor]-err[motor]);
+      err_prev[motor] = err[motor];
+    }
+    if(result[motor] > maxPWM){
+      result[motor] = maxPWM;
+    }
+    if(result[motor] < -maxPWM){
+      result[motor] = -maxPWM;
+    }
+    pwmRef[motor] = result[motor];
+  }
+  for(int motor=0;motor<2;motor++){
+    Serial.printf("pwmRef: %d positions: %d velocities: %d currents: %d\n",
+       pwmRef[motor], position[motor], velocity[motor], current[motor]
+    );
+  }
+}
+
 void loop() {
   readDeviceState(0xF, &state);
   Serial.print(F("    Angle:  "));
   Serial.println(state.angle);
   // if (nh.connected()) {
-    rosmsg.position[0] = state.angle;
-    elbow_pub.publish(&rosmsg);
-    nh.spinOnce();
+
+  rosmsg.position[0] = state.angle;
+  elbow_pub.publish(&rosmsg);
+
+  myobrickLoop();
+
+  nh.spinOnce();
   // } else {
   //   delay(10000);
   //   ESP.restart();
@@ -183,5 +322,5 @@ void loop() {
   //
   // }
 
-  delay(500);
+  delay(10);
 }
